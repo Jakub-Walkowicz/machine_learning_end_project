2debd484eedb8d95cce8ce69c68a824d548a89b0
walczykk <walkowiczjj@gmail.com>
2025-01-05 23:08:51 +0100

    initial commit

---
+++ project/constants/constants.py
@@ -0,0 +1,16 @@
+from project.constants.columns import Columns
+
+
+CATEGORICAL_COLUMNS = [
+    Columns.JOB,
+    Columns.MARITAL,
+    Columns.EDUCATION,
+    Columns.DEFAULT,
+    Columns.HOUSING,
+    Columns.LOAN,
+    Columns.CONTACT,
+    Columns.MONTH,
+    Columns.DAY_OF_WEEK,
+    Columns.POUTCOME,
+    Columns.OUTPUT,
+]
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Bank-marketing",
+    packages=find_packages()
+)
